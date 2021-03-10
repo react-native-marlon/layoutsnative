5f21c8b04c3989ed8e6122e27042853c24607246
Marlom <falconsoft.3d@gmail.com>
2021-03-10 17:07:18 +0100

    sc

--- App.js
+++ App.js
@@ -1,42 +1,131 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <>
-      <View style={styles.contenedor}>
-        <View style={styles.caja1}></View>
-        <View style={styles.caja2}></View> 
-        <View style={styles.caja3}></View> 
-        <View style={styles.caja4}></View> 
-      </View>
+     <ScrollView>
+       <View style={{ flexDirection: 'columns' }}>
+          <View>
+            <Image style={styles.banner}
+              source={ require('./assets/img/bg.jpg') }
+            />    
+          </View>
+          
+          <View style={ styles.contenedor }>
+            <Text style={ styles.titulo }>Que hacer en paris</Text>
+            
+            <ScrollView 
+            horizontal
+            >
+              <View>
+              <Image style={styles.ciudad}
+              source={ require('./assets/img/actividad1.jpg') }
+              />  
+              </View>
+              <View>
+              <Image style={styles.ciudad}
+              source={ require('./assets/img/actividad2.jpg') }
+              />  
+              </View>
+              <View>
+              <Image style={styles.ciudad}
+              source={ require('./assets/img/actividad3.jpg') }
+              />  
+              </View>
+              <View>
+              <Image style={styles.ciudad}
+              source={ require('./assets/img/actividad4.jpg') }
+              />  
+              </View>
+              <View>
+              <Image style={styles.ciudad}
+              source={ require('./assets/img/actividad5.jpg') }
+              />  
+              </View>
+            </ScrollView>
+
+            <Text style={ styles.titulo }>Los mejores alojamiento</Text>
+
+            <View>
+              <Image style={styles.mejores}
+              source={ require('./assets/img/mejores1.jpg') }
+              />  
+            </View>
+            <View>
+              <Image style={styles.mejores}
+              source={ require('./assets/img/mejores2.jpg') }
+              />  
+            </View>
+            <View>
+              <Image style={styles.mejores}
+              source={ require('./assets/img/mejores3.jpg') }
+              />  
+            </View>
+
+          </View>
+
+          <Text style={ styles.titulo }>Hospedaje en los angeles</Text>
+          
+          <View style={styles.listado}>
+            <View>
+                <Image style={styles.ListadoItem}
+                source={ require('./assets/img/hospedaje1.jpg') }
+                />  
+            </View>
+            <View>
+                <Image style={styles.ListadoItem}
+                source={ require('./assets/img/hospedaje2.jpg') }
+                />  
+            </View>
+            <View>
+                <Image style={styles.ListadoItem}
+                source={ require('./assets/img/hospedaje3.jpg') }
+                />  
+            </View>
+            <View>
+                <Image style={styles.ListadoItem}
+                source={ require('./assets/img/hospedaje4.jpg') }
+                />  
+            </View>
+          </View>
+
+        </View>
+       </ScrollView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  banner: {
+    height: 250,
+    flex: 1
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20
+  },
   contenedor: {
-    backgroundColor: 'cyan',
-    flex: 1,
-    // flexDirection: 'row',
-    // alignItems: 'flex-start'
-    justifyContent: 'center'
+    marginHorizontal: 10
   },
-  caja1: {
-    padding:20,
-    backgroundColor: '#A32446',
+  ciudad:{
+    width: 250,
+    height: 300,
+    marginRight: 10
   },
-  caja2: {
-    padding:20,
-    backgroundColor: '#A39624',
+  mejores:{
+    width: '100%',
+    height: 200,
+    marginVertical: 5,
   },
-  caja3: {
-    padding:20,
-    backgroundColor: '#762500',
+  listado:{
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   },
-  caja4: {
-    padding:20,
-    backgroundColor: '#0B6A06',
+  ListadoItem: {
+    flexBasis: '49%'
   }
+  
 });
